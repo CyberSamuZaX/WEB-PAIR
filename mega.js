3535e8a9b920a11017af23933cee61b5b70a8b09
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-03-28 07:35:45 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'cybersamuza@gmail.com',
-    password: 'Paka200411',
+    email: 'pasindusbandara923@gmail.com',
+    password: 'Thero20041113@@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
